3676aeb624d8850dd11de43511e24d3b5a0288c4
lumbardhgashi <93322621+lumbardhgashi@users.noreply.github.com>
2022-04-27 11:15:40 +0200

    Add files via upload

--- Aeroporti.sql
+++ Aeroporti.sql
@@ -1,4 +1,4 @@
-Create database Aeroporti
+﻿Create database Aeroporti
 
 use Aeroporti
 
@@ -713,3 +713,798 @@ Where ID_Rritur = 'P01'
 
 Delete From Testimi
 Where Score between '40' and '50'
+
+
+
+
+
+--Pjesa Tret
+
+
+/* 8 query të thjeshta 
+Lumbardh gashi : 2,4,6,8
+Egzon Pajaziti : 1,3,5,7
+*/
+
+/*
+1)
+Te kthehet testi me emrin "Emri2"
+*/
+Select *
+From Testi
+Where Emri = 'Emri2'
+
+/*
+2)
+Te kthehet testimi i aeroplanit 
+dhe numri regjistrimit te aeroplaneve 
+qe jane testuar ne oren 2:25
+*/
+Select * 
+From Testimi
+Where Oret = '2:25'
+
+/*
+3)
+Te ktheht Kompania per mirembajtjen e aeroplaneve
+Qe daten e licencimit e ka me '05/08/2011'
+*/
+Select *
+From Kompania_EA
+Where DataLicencimit = '05/08/2011'
+
+
+/*
+4)
+Te kthehen modelet e aeroplaneve te cilet jane
+te akomoduar ne aeroportin Adem Jashari4
+*/
+Select *
+From Akomoduar
+Where Emri_Aeroportit = 'Adem Jashari4'
+
+/*
+5)
+Te kthehet historiku i udhetimeve 
+te klientit me passaport 'P01'
+*/
+Select VendiKuDoUdhetoj
+From Udheton
+Where Pass = 'P01'
+
+/*
+6)
+Te kthehen punonjesit qe e kan 
+pagen me te vogel se 1600 $
+*/
+Select p.Emri, p.Paga, p.Qyteti
+From Punonjesi p
+Where Paga < '1600'
+
+/*
+7)
+Te kthehen aeroportet se ku punon Punetori me SSN 'S04'
+*/
+Select *
+From Punon
+Where SSN = 'S04'
+
+/*
+8)
+Te kthehen kontrolluesit e trafikut te cilet daten e ekzaminimit mjeksor
+ne mes te datave '2/2/2021' Dhe '4/1/2021'
+*/
+Select *
+From KontrolluesTrafiku
+Where DataEkzaminimitMjeksor between '2/2/2021' and '4/1/2021'
+
+
+/* 8 query të thjeshta, minimum dy relacione(tabela) e më tepër. 
+Lumbardh gashi : 1,3,5,7
+Egzon Pajaziti : 2,4,6,8
+*/
+
+/*
+1)
+Te kthehen te gjithe aeroplanet te cilet numri i pikeve nga testimi i FAA
+eshte me i vogel se 67
+*/
+Select *
+From Testi t, Testimi ts 
+Where t.FAA_Test_No = ts.FAA and ts.Score < '67'
+
+/*
+2)
+Te kthehen Aeroportet qe i mirmban kompania me id"MEA01"
+*/
+Select *
+From KompaniaPerMirembajtjenAiroportit km, MirembajtjaAiroportit ma
+Where km.ID_MEA = ma.ID_mea and km.ID_MEA = 'MEA01'
+
+/*
+3)
+Te  kthehen punetoret qe paga e tyre eshte me e vogel ose e 
+barabart me 1234 $ si dhe aeroporti ne te cilin ata punojne 
+*/
+Select a.Emri, p.Paga, p.Qyteti
+From Punonjesi p, Punon pu, Airoporti a
+Where p.SSN = pu.SSN and pu.Emri_Aeroportit = a.Emri 
+and p.Paga <= '1234'
+
+/*
+4)
+Te kthehen modelet e aeroplaneve te cilet kank peshen 58 tone kg
+dhe airoporti ku jan akomoduar ata aeroplan respektiv
+*/
+Select a.Emri_Aeroportit [Emri], m.KapaciteiAeroplanit, m.Pesha
+From Modeli m, Akomoduar a
+Where m.Numri_Modelit = a.Numri_Model and m.Pesha = '58 T'
+
+/*
+5)
+Te kthehen klientet te cilet do te aterojne per ne kosove
+*/
+Select *
+From Klienti k, Udheton u, Modeli m
+Where k.Passaporta = u.Pass and u.Nr_MOD = m.Numri_Modelit 
+and u.VendiKuDoUdhetoj = 'Kosove'
+
+/*
+6)
+Te kthehen aeroplanet te cilet pesha e tyre eshte 66 Tone kg
+*/
+Select p.NR_MODEL [Modeli], m.Pesha, p.Nr_RegjistrimAE [Regjistrimi]
+From Modeli m, Perket p
+Where m.Numri_Modelit = p.NR_MODEL and m.Pesha = '66 T'
+
+/*
+7)
+Te kthehen kompanit per mirembajten e aeroplaneve 
+qe daten e mbarimit te licences se tyre e kan deri ne  vitin 2026
+*/
+Select *
+From Kompania_EA kea, MirembajtjaEAeroplaneve ma
+Where kea.ID_MA = ma.IDKompania_EA and kea.DataEMbarimitLicences <= '2026/1/1'
+
+
+/*
+8)
+Te kthehen numrat e telefonit per punonjesin me Emrin
+"Emri07"
+*/
+Select *
+From Punonjesi p, NumriTelefonit nt
+Where p.SSN = nt.SSN_A
+and p.Emri = 'Emri07'
+
+
+
+/*
+Të krijoni min. 8 query të avancuara (4 për student), të realizuara në minimum dy relacione
+(tabela) e më tepër.
+Shënim: Query ose Subquery të avancuar konsiderohet përdorimi i kombinimeve të funksioneve
+akumuluese,respektivisht joins-ave, përkatësisht të realizohet një kërkesë/raport më kompleks.
+
+Lumbardh gashi : 2,4,6,8
+Egzon Pajaziti : 1,3,5,7
+*/
+
+/*
+1)
+Te kthehen te gjith punonjesit me pagat e tyre dhe vendin e punes si dhe 
+nga cili Qytet vjen secili punonjes po ashtu  
+te renditen sipas pages me te larte
+Le te Perdoret join
+*/
+Select a.Emri as Emri_Aeroportit,p.Emri as Emri_Puntorit , p.Paga  , p.Qyteti
+From Punonjesi p join  Punon pu  
+on p.SSN = pu.SSN  
+Join Airoporti a
+on a.Emri = pu.Emri_Aeroportit
+order by p.Paga desc
+
+
+/*
+2)
+Te Kthehen sa punetore marrin paga te njejta dhe te kthehen pagat pa u perseritur
+Le te perdoret Left Join 
+*/
+Select count(p.Paga)[Punetoret] ,p.Paga
+From Punonjesi p Left Join Punon pu 
+on p.SSN = pu.SSN
+Group by p.Paga 
+
+
+/*
+3)
+Te kthehen aeroplanet me numrin e regjistrimit te tyre ,
+piket dhe daten e testimit 
+duke pasur parasysh qe duhet te kthehet secila her qe nje aeroplan eshte testuar
+te radhiten nga aeroplani me piket e testimit me te medha e deri tek ai me i vogli 
+Le te perdoret inner Join dhe Right Join 
+*/
+Select a.Numri_regjistrimit , t.Score [Piket_Nga_Testimi] , t.Data_Testimit
+From Testi te inner Join Testimi t 
+on te.FAA_Test_No=t.FAA
+Right join Aeroplani a
+on a.Numri_regjistrimit = t.Nr_Regjistrimit
+order by t.Score desc
+
+/*
+4)
+Te kthehen Aeroplanet se nga cila Kompani mirmbahen 
+dhe secila her qe kompania e ka mirmbajt aeroplanin respektiv
+Perdorimi i Right Join dhe Full Outer Join inclusive
+*/
+Select*
+From Kompania_EA ke Right Join MirembajtjaEAeroplaneve me
+on ke.ID_MA = me.IDKompania_EA Full outer join Aeroplani ae
+on me.Nr_RegjAeroplanit = ae.Numri_regjistrimit
+
+/*
+5)
+Te kthehet numri i punonjeseve se sa punojes punojne ne Aeroportet e caktuara
+Dhe te radhiten nga  Aeroporti me me shum punetor tek aeroporti me me se paku punetor
+Perdorimi i Left Join
+*/
+Select count(p.SSN)[Numri_Puntorve] , ae.Emri [Emri_Aeroportit]
+From Punon p Left Join Airoporti ae
+On p.Emri_Aeroportit = ae.Emri
+Group by ae.Emri
+order by Numri_Puntorve desc
+
+
+/*
+6)
+Te kthehen modelet e aeroplaneve te akomuduar ne aeroportet respektive
+te cilet pesha e tyre eshte me e larte se 58 Ton Kg
+Perdorimi i Full Outer JOIN 
+*/
+Select a.Emri_Aeroportit [Emri], m.KapaciteiAeroplanit, m.Pesha
+From Modeli m Full outer join  Akomoduar a
+on m.Numri_Modelit = a.Numri_Model
+Where  m.Pesha = '58 T'
+
+/*
+7)
+Te kthehen te gjitha informatat per klientin se ku do te shkoj dhe me 
+cilin model te aeroplaneve te akomuduar ne aeroport do te ateroj
+Perdorimi i inner Join dhe Right Join 
+*/
+Select *
+From Klienti k inner Join Udheton u
+on k.Passaporta =u.Pass 
+Right join Modeli m
+on u.Nr_MOD =m.Numri_Modelit
+Where u.VendiKuDoUdhetoj = 'Kosove' 
+
+/*
+8)
+Te kthehen punetoret te cilet punojne ne aeroporte te caktuara 
+te cilet paga e tyre eshte me e vogel se 1234$
+Perdorimi i left Join dhe Full outer join 
+*/
+Select a.Emri, p.Paga, p.Qyteti
+From Punonjesi p left Join  Punon pu
+on p.SSN = pu.SSN
+Full Outer Join  Airoporti a
+on a.Emri = pu.Emri_Aeroportit
+Where p.Paga <= '1234'
+order by p.Paga desc
+
+
+
+/*
+Të krijoni min. 8 subquery të thjeshta (4 për student).
+Lumbardh gashi : 1,3,5,7
+Egzon Pajaziti : 2,4,6,8
+*/
+
+
+/*
+1)
+Te kthehen te gjithe klientet qe nuk jane femije 
+*/
+Select *
+From  IRritur ir 
+Where ir.PassPort in (Select k.Passaporta
+                      From Klienti k)
+
+
+/*
+2)
+Te kthehen prinderit e femijeve , me ke udhetojne femijet 
+Per secilin femije 
+*/
+Select f.PassPort [Pasaporta_Femijes] ,f.ID_Rritur [Pasaporta_prindit]
+From  Femija f 
+Where f.PassPort in (Select k.Passaporta
+                      From Klienti k)
+
+/*
+3)
+Per secilin klient tregoni ku ka udhetuar 
+Historiku i fluturimeve per secilin klient
+*/
+Select u.Pass[Klienti] ,u.VendiKuDoUdhetoj 
+From Udheton u
+Where u.Pass in (Select k.Passaporta
+                 From Klienti k)
+
+
+/*
+4)
+Nga te gjithe klientet e aeroportit qe kan shfrytezuar 
+per aterime te kthehen ata klient te cilet jan femije
+*/
+Select f.PassPort
+From  Femija f 
+Where f.PassPort in (Select k.Passaporta
+                      From Klienti k)
+
+
+/*
+5)
+Te kthehen te gjithe klientet e aeroportit 
+te cilet do te udhetojne per ne kosove
+*/
+Select u.Pass [Klienti] , u.VendiKuDoUdhetoj
+From Udheton u 
+Where u.VendiKuDoUdhetoj='Kosove' and u.Pass in (Select k.Passaporta
+                                                 From Klienti k)
+
+/*
+6)
+Te kthehn aeroplanet qe jan testuar
+per te ateruar ne destinacone rrespektive
+*/
+Select *
+From Aeroplani a
+Where a.Numri_regjistrimit in (Select t.Nr_Regjistrimit
+                                   From Testimi t)
+
+/*
+7)
+Te kthehen aeroplanet e testuar 
+qe piket e tyre jane me te medha se sa piket me te vogla 
+nga testimi
+*/
+Select t.Nr_Regjistrimit[NrRegjistrimit_Aeroplanit] ,t.Score[Piket_Nga_Testimi] 
+From Testimi t 
+Where t.Score >Any (Select te.PiketMax
+                    From Testi te)
+
+/*
+8)
+Te kthehen kontrolluesit e trafikut nga te gjithe punonjesit 
+qe punojne ne aeroporte 
+*/
+Select *
+From KontrolluesTrafiku k 
+Where k.SSN in(Select p.SSN
+                From Punonjesi p) 
+
+
+
+
+
+/*
+Të krijoni min. 8 subquery të avancuara (4 për student). (min. 1 subquery në klauzolën
+SELECT, dhe min. 1 subquery ne klauzolën FROM)
+Lumbardh Gashi : 1,3,5,7,10
+Egzon Pajaziti : 2,4,6,8,9
+*/
+
+/*
+1)
+Te kthehen punetoret qe e kane pagen me te vogel se
+Paga me e madhe se e kontrollueseve te trafikut 
+dhe te mos perseriten pagat po ashtu te radhiten nga me e 
+madhja tek me e vogla
+*/
+Select Distinct p.Paga , p.Emri 
+From Punonjesi p join PunetoriTeknik pt
+on p.SSN = pt.SSN
+Where p.Paga <Any (Select p.Paga
+                   From KontrolluesTrafiku k inner Join Punonjesi p
+				   on k.SSN = p.SSN)
+order by p.Paga desc
+
+/*
+2)
+Te kthehen punonjesit qe kan 3 
+numra te telefonit ose me shume
+dhe te rradhiten nga me e madhja tek me e vogla.
+*/
+Select p.SSN , count(Nr_Tel)[Numrat e telefonit]
+From Punonjesi p Right Join NumriTelefonit nr
+on p.SSN = nr.SSN_A 
+Group by p.SSN 
+Having count(nr.Nr_Tel) in (Select Count(nr.Nr_Tel)[nr] 
+                              From NumriTelefonit nr inner Join Punonjesi p
+							  ON nr.SSN_A = p.SSN
+							  Group by p.SSN
+							  Having Count(nr.Nr_Tel) >= 3
+							  )
+order by [Numrat e telefonit] desc
+
+
+/*
+3)
+Te kthehen klientet qe udhetojne per 
+Ne Zvicer
+*/
+Select *
+From Klienti k Full Outer Join Udheton u
+on k.Passaporta = u.Pass
+Where u.VendiKuDoUdhetoj in (Select u.VendiKuDoUdhetoj
+                             From Klienti k Right Join Udheton u
+							 on k.Passaporta = u.VendiKuDoUdhetoj
+							 Where u.VendiKuDoUdhetoj = 'Zvicer')
+
+
+/*
+4)
+Te kthehen modelet e aeroplaneve te cilet nuk udhetojne 
+por vetem jane te akomoduar ne aeroporte te caktuara
+*/
+Select m.Numri_Modelit[Modeli Aeroplanit ] ,m.KapaciteiAeroplanit , m.Pesha , a.Emri_Aeroportit[Aeroplanet e Akomoduar] 
+From Modeli m Inner Join Akomoduar a
+on m.Numri_Modelit = a.Numri_Model
+Where m.Numri_Modelit in (Select m.Numri_Modelit
+                              From Modeli m Left Join Udheton u
+                              on m.Numri_Modelit = u.Nr_MOD
+							  Where u.VendiKuDoUdhetoj is null)
+/*
+5)
+Te kthehen modelet e aeroplaneve qe 
+kontrollohen nga puntoret e caktuar teknik
+ku ato modele udhetojne per ne kosove
+*/
+Select m.Numri_Modelit[Modeli Aeroplanit], m.KapaciteiAeroplanit , m.Pesha ,pt.SSN[Punetoret teknik]
+From Modeli m Left Join PunetoriTeknik pt
+on m.Numri_Modelit = pt.Numri_Modelit
+Where m.Numri_Modelit =Any (Select m.Numri_Modelit
+                            From Modeli m Right Join Udheton u
+							On m.Numri_Modelit = u.Nr_MOD
+							Where u.VendiKuDoUdhetoj='Kosove')
+
+
+/*
+6)
+Te kthehen cilat aeroporte i miremban kompania
+Me ID 'MEA01'
+*/
+Select kp.ID_MEA[Kompania me id] , kp.DataLicencimit ,kp.DataMbarimitLicences , ma.Emri_Aeroportit
+From KompaniaPerMirembajtjenAiroportit kp Join MirembajtjaAiroportit ma
+on kp.ID_MEA =ma.ID_mea
+Where kp.ID_MEA  in (Select kp.ID_MEA
+                    From MirembajtjaAiroportit ma Right Join Airoporti a
+					On ma.Emri_Aeroportit = a.Emri
+					inner Join  KompaniaPerMirembajtjenAiroportit kp
+					on kp.ID_MEA = ma.ID_mea
+					Where kp.ID_MEA = 'MEA01')
+
+/*
+7)
+Te kthehen modelet e aeroplaneve te cilet 
+udhetojne dhe bartin klient si dhe kapacitetet e tyre por 
+edhe nga cilet punetore teknik mirembahen ata aeroplan 
+*/
+Select  m.Numri_Modelit [Modeli Aeroplanit] , m.KapaciteiAeroplanit , m.Pesha , pt.SSN[Puntori Qe mbikqyre Aeroplanin] 
+From Modeli m  Right join PunetoriTeknik pt
+on m.Numri_Modelit = pt.Numri_Modelit
+Where m.Numri_Modelit  not in (Select m.Numri_Modelit
+                               From Modeli m Left Join Udheton u
+							   On m.Numri_Modelit = u.Nr_MOD
+							   Where u.VendiKuDoUdhetoj is null)
+
+/*
+8)
+Te kthehen Aeroplanet Te cilet te cilet jane 
+Testuar me shume se 2 her 
+*/
+Select te.Nr_Regjistrimit , te.Data_Testimit ,te.Score [Piket Nga Testimi]
+From Testi t Join Testimi te
+on t.FAA_Test_No = te.FAA
+Where te.Nr_Regjistrimit in (Select a.Numri_regjistrimit
+                         From Aeroplani a Right join Testimi te
+						 on a.Numri_regjistrimit = te.Nr_Regjistrimit
+						 Group by a.Numri_regjistrimit
+						 Having Count(te.FAA) > 2)
+Order by te.Nr_Regjistrimit asc
+
+/*
+9)
+Te kthehen nga sa numra i kan punetoret tane 
+Duke e perdorur subquery ne klauzolen Select
+*/
+Select p.Emri,p.SSN[ID Puntori],Telefoni =
+      (Select Count(*)
+       From NumriTelefonit nr 
+	   Where nr.SSN_A = p.SSN)
+From Punonjesi p
+
+/*
+10)
+Te kthehen sa her jan testuar te gjithe aeroplanet 
+Duke Perdorur SubQuery Ne Klauzolen From
+*/
+Select a.Numri_regjistrimit , Subquery.[Sa her esht testu]
+From Aeroplani a ,(Select t.Nr_Regjistrimit , count(*) [Sa her esht testu]
+                  From Testimi t 
+				  Group by t.Nr_Regjistrimit) Subquery
+Where Subquery.Nr_Regjistrimit = a.Numri_regjistrimit
+
+
+/*
+▪ Të krijoni min. 8 query/subquery (4 për student). Duke përdorur operacionet e algjebrës
+relacionale (Union, Prerja, diferenca, etj.)
+Lumbardh Gashi : 2,4,6,8
+Egzon Pajaziti : 1,3,5,7
+*/
+
+/*
+1)
+Te kthehen modelet e aeroplaneve te cilet kank peshen 58 tone kg
+dhe airoporti ku jan akomoduar ata aeroplan respektiv
+*/
+π a . emri_aeroportit, m . kapaciteiaeroplanit, m . pesha
+ σ m . numri_modelit = a . numri_model AND m . pesha = "58 T"
+  (ρ m modeli ×
+   ρ a akomoduar)
+
+/*
+2)
+Te kthehen te gjithe aeroplanet te cilet numri i pikeve nga testimi i FAA
+eshte me i vogel se 67
+*/
+σ t . faa_test_no = ts . faa AND ts . score < "67"
+ (ρ t testi ×
+  ρ ts testimi)
+
+
+/*
+3)
+Te kthehen numrat e telefonit per punonjesin me Emrin
+"Emri07"
+*/
+σ p . ssn = nt . ssn_a AND p . emri = "Emri07"
+ (ρ p punonjesi ×
+  ρ nt numritelefonit)
+
+
+/*
+4)
+Te kthehen modelet e aeroplaneve te cilet jane
+te akomoduar ne aeroportin Adem Jashari4
+*/
+σ emri_aeroportit = "Adem Jashari4" akomoduar
+
+
+/*
+5)
+Te kthehen kompanit per mirembajten e aeroplaneve 
+qe daten e mbarimit te licences se tyre e kan deri ne  vitin 2026
+*/
+σ kea . id_ma = ma . idkompania_ea AND kea . dataembarimitlicences <= "2026/1/1"
+ (ρ kea kompania_ea ×
+  ρ ma mirembajtjaeaeroplaneve)
+
+/*
+6)
+Te kthehen numrat e telefonit per punonjesin me Emrin
+"Emri07"
+*/
+σ p . ssn = nt . ssn_a AND p . emri = "Emri07"
+ (ρ p punonjesi ×
+  ρ nt numritelefonit)
+
+/*
+7)
+Te kthehen kontrolluesit e trafikut te cilet daten e ekzaminimit mjeksor
+ne mes te datave '2/2/2021' Dhe '4/1/2021'
+*/
+σ "2/2/2021" <= dataekzaminimitmjeksor AND dataekzaminimitmjeksor <= "4/1/2021" kontrolluestrafiku
+
+
+/*
+8)
+Te kthehen aeroportet se ku punon Punetori me SSN 'S04'
+*/
+σ ssn = "S04" punon
+
+
+
+
+/*
+Të krijoni min. 8 Proceduara të ruajtura (Stored Procedure) (4 për student)
+▪ Minimum 2 procedura me parametra input dhe output
+▪ Min. 2 procedura me kombinime të shprehjeve (IF…ELSE, WHILE, CASE )Lumbardh Gashi : 1,3,5,7
+Egzon Pajaziti : 2,4,6,8
+*/
+
+/*
+1)
+Te kthehen punonjesit qe e kan emrin 'Emri01' 
+ose qe e kan pagen 4000 $
+Me ane te Stored Procedure 
+Duke perdorur inpute nga shfytezuesi
+*/
+Create Procedure Proc1
+@Emri varchar ,
+@Paga money
+as
+Begin
+Select*
+From Punonjesi p
+Where p.Emri = @Emri or
+p.Paga = @Paga
+End
+Drop Procedure Proc1
+
+Exec Proc1 'Emri01' ,'4000'
+
+/*
+2)
+Te kthehet kompania qe daten e licencimit 
+e merr nga shfrytezuesi 
+*/
+Create Procedure Proc2
+@DataLicencimit date 
+as
+Begin
+Select *
+From Kompania_EA
+Where DataLicencimit = @DataLicencimit
+end
+
+Exec Proc2 '05/08/2011'
+
+
+/*
+3)
+Te kthehen pagat  punetoreve nga vijne si dhe pagen le ta merr
+nga shfrytezuesi (input ) po ashtu te kemi 2 outpute Qytetin dhe emrin 
+e aeroportit ne te cilin punojne 
+JAN PERDORUR EDHE OUTPUTET 
+*/
+Create Procedure Proc3
+@Emri Varchar output,
+@Paga money  ,
+@Qyteti varchar output
+as
+Begin
+Select a.Emri, p.Paga, p.Qyteti
+From Punonjesi p, Punon pu, Airoporti a
+Where p.SSN = pu.SSN and pu.Emri_Aeroportit = a.Emri 
+and p.Paga <=@Paga 
+end
+
+DECLARE @Emri varchar, @Paga money, @Qyteti varchar
+Exec Proc3 
+@Emri out,
+'4000',
+@Qyteti out
+Print 'Puntori me Emer : '+@Emri+' me page :' + convert (varchar,@Paga)+' nga :'+@Qyteti
+
+/*
+4)
+Te kthehn klientet
+qe daten e lindjes e kan me te madhe se 
+data e dhene nga shfrytezuesi ose
+te jet personi i cili ka pasaporten e njejt me 
+ate qe e shtyp Shfrytezuesi
+*/
+
+Create Procedure Proc4
+@DataLindjes date,
+@Pass char
+AS
+Begin
+Select *
+From Klienti k
+Where k.DataLindjes > @DataLindjes
+or k.Passaporta =@Pass
+End
+Exec Proc4 '2001-12-24' ,'P01'
+
+
+
+/*
+5)
+Te kthehen te gjithe punonjesit te cilet
+vin nga qyteti qe i jep shfrytezuesi ose
+te cilet pagen e kan me te madhe se ajo qka 
+jep shfytezuesi
+*/
+Create Procedure Proc5
+@Qyteti varchar,
+@Paga money
+AS
+Begin
+Select *
+From Punonjesi p
+Where p.Qyteti =@Qyteti or 
+p.Paga > @Paga
+End
+Exec Proc5 'Qyteti1','3000'
+
+/*
+6)
+Te kthehen Sa puntor jan te 
+Atij Lloji qe kerkon shfrytezuesi 
+*/
+Create Procedure Proc6
+(
+@LlojiPuntorit varchar(50),
+@Numro int = null
+)
+AS
+begin 
+   if(@LlojiPuntorit = 'PuntorTeknik')
+ begin 
+   Set @Numro =(
+   Select Count(*)
+   From PunetoriTeknik p join Punonjesi pu
+   on p.SSN=pu.SSN)
+   Print 'Puntor Teknik jan gjithsej : '+convert(varchar,@Numro)
+ end
+   else 
+   if(@LlojiPuntorit ='KontrolluesTrafiku')
+   begin 
+    Set @Numro =(
+    Select Count(*)
+    From KontrolluesTrafiku kT Inner Join Punonjesi pu
+    on kT.SSN = pu.SSN)
+    Print 'Kontrollues Trafiku jan gjithsej : '+convert(varchar,@Numro)
+   end
+    else Print 'Nuk posedojm ket lloj te puntorit'
+end
+
+Drop Procedure Proc6
+
+Exec Proc6 'KontrolluesTrafiku'
+
+
+/*
+7)
+Te kthehet puntori me mbshkrim nese ka 
+pagen e mire apo jo i cili shfrytezuesi e dergon SSN e 
+puntorit si dhe pagen e tij 
+*/
+Create PROCEDURE Proc7
+@SSN char(5)=null,
+@Paga int = null
+AS
+BEGIN
+SELECT p.Emri , p.Qyteti,p.Rruga,p.SSN ,Paga=( CASE
+WHEN @Paga between 500 and 1200 THEN 'Punonjesi ka page te Ulet'
+WHEN @Paga between 1210 and 10000 THEN 'Punonjesi ka page te mire '
+else NULL
+END )
+FROM Punonjesi p 
+Where p.SSN = @SSN
+Group by p.Emri , p.Qyteti,p.Rruga,p.SSN 
+END
+
+
+execute Proc7 'S01', 4000
+
+
+/*
+8)
+Te Kthehen 10 punonjesit e pare qe punojne ne 
+aeroporte duke i kthyer si tabela
+whlie loop
+*/
+DECLARE @Numri INT = 0 ;
+DECLARE @Totali INT = 0 ;
+WHILE @Numri < = 8
+BEGIN
+SET @Totali = @Totali + @Numri;
+SET @Numri = @Numri + 1 ;
+Select *
+From Punonjesi
+Where SSN like 'S0'+convert(varchar,@Numri)
+END
